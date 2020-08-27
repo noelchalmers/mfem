b0a228d1a958ff73ffd04cec18a5975e788c5dd7
Noel Chalmers <noel.chalmers@amd.com>
2020-08-27 12:34:25 -0500

    Small changes for ROCm 3.5

--- linalg/kernels.hpp
+++ linalg/kernels.hpp
@@ -204,10 +204,12 @@ void MultABt(const int Aheight, const int Awidth, const int Bheight,
 /// the eigenvalues in the array @a lambda and the eigenvectors in the array @a
 /// vec (listed consecutively).
 template<int dim>
+MFEM_HOST_DEVICE
 void CalcEigenvalues(const double *data, double *lambda, double *vec);
 
 /// Return the i'th singular value of the matrix of size dim with given @a data.
 template<int dim>
+MFEM_HOST_DEVICE
 double CalcSingularvalue(const double *data, const int i);
 
 
